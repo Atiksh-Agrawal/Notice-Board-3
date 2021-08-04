e8ea6542f4700ba7928fc0b7300ee10bd63fb6c9
Atiksh-Agrawal <74902465+Atiksh-Agrawal@users.noreply.github.com>
2021-08-04 07:59:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseY = 0;
+noseX = 0;
+
+difference = 0;
+rightHandX = 0;
+leftHandX = 0;
+
+function setup(){
+    canvas = createCanvas(550,500);
+    canvas.position(600,200);
+    video = createCapture(VIDEO);
+    video.size(400,400);
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+
+    poseNet.on('pose',getPoses);
+}
+
+function draw(){
+   background("thistle");
+   textSize(difference);
+   fill("red");
+
+   text("Atiksh", noseX, noseY);
+}
+
+function modelLoaded(){
+    console.log("posenet is initialized");
+}
+
+function getPoses(error,results){
+    
+    
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+        rightHandX = results[0].pose.rightWrist.x;
+        leftHandX = results[0].pose.leftWrist.x;
+        difference = floor (leftHandX - rightHandX);
+    }
+}
